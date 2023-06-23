426f43555145a651c59a9fac85d760ba5f7eeb63
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-06-23 14:55:28 +0700

    membuat constructor

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -6,5 +6,16 @@ class Queues {
 	int front, rear, max = 5;
 	int queue_array[5];
 
+public:
+	Queues() {
+		front = -1;
+		rear = -1;
+	}
 
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+	}
 };
